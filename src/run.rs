60183122c87de4930aac2f9fb603c5daa9f5f44c
TheBlckbird <theblckbrd@proton.me>
2024-06-09 19:12:54 +0200

    Remove redundant code

--- src/run.rs
+++ src/run.rs
@@ -1,21 +1,17 @@
 use crate::ast::{BinOp, Expr, UnOp};
 
-pub fn run(ast: Expr) -> f64 {
-    eval_expr(ast, 0.0)
-}
-
-fn eval_expr(expr: Expr, result: f64) -> f64 {
+fn run(expr: Expr) -> f64 {
     match expr {
         Expr::BinOp { left, right, op } => match op {
-            BinOp::Add => eval_expr(*left, result) + eval_expr(*right, result),
-            BinOp::Sub => eval_expr(*left, result) - eval_expr(*right, result),
-            BinOp::Mul => eval_expr(*left, result) * eval_expr(*right, result),
-            BinOp::Div => eval_expr(*left, result) / eval_expr(*right, result),
-            BinOp::Mod => eval_expr(*left, result) % eval_expr(*right, result),
-            BinOp::Pow => eval_expr(*left, result).powf(eval_expr(*right, result)),
+            BinOp::Add => run(*left) + run(*right),
+            BinOp::Sub => run(*left) - run(*right),
+            BinOp::Mul => run(*left) * run(*right),
+            BinOp::Div => run(*left) / run(*right),
+            BinOp::Mod => run(*left) % run(*right),
+            BinOp::Pow => run(*left).powf(run(*right)),
         },
         Expr::UnOp(expr, op) => match op {
-            UnOp::Neg => -eval_expr(*expr, result),
+            UnOp::Neg => -run(*expr),
         },
         Expr::Number(num) => num,
     }
